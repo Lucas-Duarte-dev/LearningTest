8365d196f9b48bf08aed9aeca07d04f0bf29e7c0
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-11-17 22:53:15 -0300

    feat(test): iniciando o aprendizado de teste com jest

---
+++ src/lib/calculetor.spec.js
@@ -0,0 +1,10 @@
+const {sum} = require('./calculetor');
+
+it("Should sum 2 and and 2 the result must be 4 ", () => {
+    // Prepare
+
+    // Execute
+
+    // Expect
+    expect(sum(2, 2)).toBe(4);
+})
