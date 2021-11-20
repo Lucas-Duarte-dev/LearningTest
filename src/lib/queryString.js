29793fb8cf6115fe854892d69f0c33a77c21e04f
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-11-19 22:12:44 -0300

    feat(lib): criando o método para realizar o parse de uma queryString para um objeto

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,5 +1,28 @@
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check your params');
+  }
+
+  return `${key}=${value}`;
+};
+
+const stringToKeyValue = (item) => {
+  let [key, value] = item.split('=');
+
+
+  if (value.indexOf(',') > -1) {
+    value = value.split(',');
+  }
+
+  return [key, value];
+}
+
 module.exports.queryString = obj => {
   return Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
+    .map(keyValueToString)
     .join('&');
 };
+
+module.exports.parse = string => {
+  return Object.fromEntries(string.split("&").map(stringToKeyValue));
+}
