3a423fe4a44f7e08a7776feac11c9912399b8c13
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-11-21 19:27:28 -0300

    refactor: adicionando o método getAmount no getTotal para pegar os valores retornandos pela lib Dinero

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -1,65 +1,110 @@
 import Cart from './Cart';
 
 describe('Cart', () => {
-    let cart;
-    let product = {
-      title: 'Adidas running shoes - men',
-      price: 35388
-    };
+  let cart;
+  let product = {
+    title: 'Adidas running shoes - men',
+    price: 35388,
+  };
 
-    let product2 = {
-      title: 'Adidas running shoes - women',
-      price: 41872
-    };
+  let product2 = {
+    title: 'Adidas running shoes - women',
+    price: 41872,
+  };
 
-    beforeEach(() => {
-       cart = new Cart();
-    });
+  beforeEach(() => {
+    cart = new Cart();
+  });
 
+  describe('getTotal()', () => {
     it('Should return 0 when getTotal() is execute in newly created instance', () => {
-        const cart = new Cart();
+      const cart = new Cart();
 
-        expect(cart.getTotal()).toEqual(0);
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
 
     it('Should multiply quantity and price and receive the total amount', () => {
-        const item = {
-           product,
-           quantity: 2
-       };
+      const item = {
+        product,
+        quantity: 2,
+      };
 
-       cart.add(item);
+      cart.add(item);
 
-       expect(cart.getTotal()).toEqual(70776);
+      expect(cart.getTotal().getAmount()).toEqual(70776);
     });
 
     it('Should ensure no more than on product exists at a time', () => {
       cart.add({
         product,
-        quantity: 2
+        quantity: 2,
       });
 
       cart.add({
         product,
-        quantity: 1
+        quantity: 1,
       });
 
-      expect(cart.getTotal()).toEqual(35388);
+      expect(cart.getTotal().getAmount()).toEqual(35388);
     });
 
     it('Should update total when a product gets included and then removed', () => {
       cart.add({
         product,
-        quantity: 2
+        quantity: 2,
       });
 
       cart.add({
         product: product2,
-        quantity: 1
+        quantity: 1,
       });
 
       cart.remove(product);
 
-      expect(cart.getTotal()).toEqual(41872);
-    })
+      expect(cart.getTotal().getAmount()).toEqual(41872);
+    });
+  });
+
+  describe('checkout()', () => {
+    it('Should return an object with the total the list of items', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    it('Should return an object with the total the list of items when summary() is called', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      expect(cart.summary()).toMatchSnapshot();
+      expect(cart.getTotal().getAmount()).toBeGreaterThan(0); // Espero que seja maior que (0);
+    });
+
+    it('Should reset the cart when checkout() is called', () => {
+      cart.add({
+        product,
+        quantity: 3,
+      });
+
+      cart.checkout();
+
+      expect(cart.getTotal().getAmount()).toEqual(0);
+    });
+
+  });
 });
