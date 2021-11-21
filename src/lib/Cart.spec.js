e2660350be329bc32f34e2da1dc8012ba9c409aa
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-11-20 21:07:44 -0300

    feat(test): fazendo uso do TDD
    crindo testes para os métodos da classe Cart: (add, remove, getTotal)

---
+++ src/lib/Cart.spec.js
@@ -0,0 +1,65 @@
+import Cart from './Cart';
+
+describe('Cart', () => {
+    let cart;
+    let product = {
+      title: 'Adidas running shoes - men',
+      price: 35388
+    };
+
+    let product2 = {
+      title: 'Adidas running shoes - women',
+      price: 41872
+    };
+
+    beforeEach(() => {
+       cart = new Cart();
+    });
+
+    it('Should return 0 when getTotal() is execute in newly created instance', () => {
+        const cart = new Cart();
+
+        expect(cart.getTotal()).toEqual(0);
+    });
+
+    it('Should multiply quantity and price and receive the total amount', () => {
+        const item = {
+           product,
+           quantity: 2
+       };
+
+       cart.add(item);
+
+       expect(cart.getTotal()).toEqual(70776);
+    });
+
+    it('Should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2
+      });
+
+      cart.add({
+        product,
+        quantity: 1
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('Should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
+    })
+});
