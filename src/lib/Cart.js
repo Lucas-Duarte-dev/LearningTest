22618a6774fb45e5fae81ee101109436146a0dac
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-11-23 22:10:45 -0300

    feat(lib): adicionando método para verificar o melhor desconto com base em uma lista de condições

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -9,11 +9,26 @@ const calculatePercentageDiscount = (amount, item) => {
   return Money({amount: 0});
 }
 const calculateQuantityDiscount = (amount, item) => {
+  const isEven = item.quantity % 2 === 0;
   if (item.condition?.quantity && item.quantity > item.condition.quantity) {
-    return amount.percentage(50);
+    return amount.percentage(isEven ? 50 : 40);
   }
 
   return Money({amount: 0});
+}
+
+const calculateDiscount = (amount, quantity, condition) => {
+  const list = Array.isArray(condition) ? condition : [condition];
+
+  const [higherDiscount] = list.map(cond => {
+    if (cond.percentage) {
+      return calculatePercentageDiscount(amount, {condition: cond, quantity}).getAmount();
+    } else if (cond.quantity) {
+      return calculateQuantityDiscount(amount, {condition: cond, quantity}).getAmount();
+    }
+  }).sort((a, b) => b - a);
+
+  return Money({amount: higherDiscount});
 }
 
 Money.defaultCurrency ='BRL';
@@ -42,12 +57,8 @@ export default class Cart {
       const amount = Money({amount:  item.quantity * item.product.price})
       let discount = Money({ amount: 0 });
 
-      if (item.condition?.percentage) {
-        discount = calculatePercentageDiscount(amount, item);
-      }
-
-      if (item.condition?.quantity) {
-        discount = calculateQuantityDiscount(amount, item);
+      if (item.condition) {
+        discount = calculateDiscount(amount, item.quantity, item.condition);
       }
 
       return acc.add(amount).subtract(discount);
